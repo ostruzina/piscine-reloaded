c3b97931e7c4ff6e0455183cb0a63e14e952a311
Veronika Svecova <vrnksvcv@gmail.com>
2026-08-19 18:35:55 +0200

    Add ex06 ft_print_alphabet

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: verosvec <verosvec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 18:19:58 by verosvec          #+#    #+#             */
+/*   Updated: 2026/08/19 18:29:26 by verosvec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		write(1, &c, 1);
+		c++;
+	}
+}
+
+void	ft_print_alphabet(void);
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
