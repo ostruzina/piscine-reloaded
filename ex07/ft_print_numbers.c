e6b6d7b00cd9b7fd7ab51abe2813fea491ef561f
Veronika Svecova <vrnksvcv@gmail.com>
2026-08-19 19:00:45 +0200

    Add ex07 ft_print_numbers

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: verosvec <verosvec@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/19 18:52:21 by verosvec          #+#    #+#             */
+/*   Updated: 2026/08/19 19:00:12 by verosvec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char	digit;
+
+	digit = '0';
+	while (digit <= '9')
+	{
+		write(1, &digit, 1);
+		digit++;
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_print_numbers();
+// 	return (0);
+// }
